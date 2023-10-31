55fa62935fbdb4aa5490890048c8550900757283
Noah Shan <lust595@gmail.com>
2023-10-31 12:44:38 +0530

    shema created.

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 app.use(bodyParser.json());
-app.get('/',(req,res,next)=>{
-    res.send('hello world!')
-});
+
+app.use(
+  "/graphql",
+  graphqlHttp.graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery{
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+`),
+    rootValue: {
+      events: () => {
+        return ["cooking", "sailing", "al-night,cording"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
+
 app.listen(5000);
