735c63f2650891996f60d5bf4e5bd368da8072b4
Noah Shan <lust595@gmail.com>
2023-11-01 20:33:15 +0530

    insert and get queries added

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/event");
 
 const app = express();
-
-const events = []; 
 
 app.use(bodyParser.json());
 
@@ -43,22 +43,47 @@ app.use(
 `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then((events) => {
+            return events.map((eve) => {
+              return { ...eve._doc, _id : eve._doc._id.toString() };
+            });
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
       createEvent: (args) => {
-        const event = {
-            _id : Math.random().toString(),
-            title: args.eventInput.title,
-            description: args.eventInput.description,
-            price : +args.eventInput.price,
-            date : args.eventInput.date
-        }
-        events.push(event);
-        return event;
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+        });
+        return event
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc };
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
       },
     },
     graphiql: true,
   })
 );
 
-app.listen(5000);
+mongoose
+  .connect(
+    `mongodb+srv://noahluthofficial:${process.env.MONGO_PASSWORD}@event-booking.jcnjpe3.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+// sloSQvsbTQmxKRuS
